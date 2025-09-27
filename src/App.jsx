1ec9f9e95f88d742fc8946be4b0e8c9bf66008d0
r1chwo0n <woonzy09@gmail.com>
2025-09-27 21:10:10 +0700

    Change: logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Layout from './Layout'
 import Projects from './pages/Projects'
-import Certificate from './pages/Certificate'
 
 export default function App() {
   return (
